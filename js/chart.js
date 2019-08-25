9c9933f4f828044c6534c596897428cae113e396
NatuO9 <nathaniel.habtegerge@ucalgary.ca>
2019-08-25 13:10:26 -0600

    added a convertRawTimes function

--- js/chart.js
+++ js/chart.js
@@ -9,11 +9,6 @@ $(document).ready(function(){
     };
     let app = firebase.initializeApp(config);
     db = firebase.firestore(app);
-
-    //firebase.firestore.setLogLevel("debug");
-
-    
-    //console.log(formatDate());
     
     db.collection("Gyms").doc("DewdmGRDsqLyxChcJCKp").collection("Usage").where("Date", "==", formatDate())
     .onSnapshot(function(querySnapshot) {
@@ -33,19 +28,17 @@ $(document).ready(function(){
         query_date(from, to, db);
     }
 
-
     $('input[name="daterange"]').daterangepicker({
         opens: 'left'
         }, function(start, end, label, db) {
             from = start.format('YYYY/MM/DD');
             to = end.format('YYYY/MM/DD');
             stuff();
-
             //query_date(start.format('YYYY/MM/DD'), end.format('YYYY/MM/DD'), db);
-        //console.log("A new date selection was made: " + start.format('YYYY/MM/DD') + ' to ' + end.format('YYYY/MM/DD'));
-        });
-
+            //console.log("A new date selection was made: " + start.format('YYYY/MM/DD') + ' to ' + end.format('YYYY/MM/DD'));
+    });
     
+
 })
 
 // a function that grabs todays date and formats it in yyyy/mm/dd
@@ -54,7 +47,7 @@ function formatDate() {
         month = '' + (d.getMonth() + 1),
         day = '' + d.getDate(),
         year = d.getFullYear();
-        
+
     if (month.length < 2) month = '0' + month;
     if (day.length < 2) day = '0' + day;
 
@@ -64,16 +57,29 @@ function formatDate() {
 function query_date(from, to, db){
     db.collection("Gyms").doc("DewdmGRDsqLyxChcJCKp").collection("Usage").where("Date", ">=", from).where("Date", "<=", to)
     .onSnapshot(function(querySnapshot) {
-        var machines = [];
-        var totalTimes = [];
-        var times = [];
+        var machines = {};
         querySnapshot.forEach(function(doc) {
-            machines.push(doc.data().Name);
-            totalTimes.push(doc.data().TotalTime);
-            times.push(doc.data().Times);
+            doc = doc.data();
+            name = doc.Name;
+            times = doc.Times;
+            machines[name] = times;
         });
-        console.log(times);
         
+        convertRawTimes(machines);
+        console.table(machines);
+        keys = Object.keys(machines);
+        values = Object.keys(machines).map(function(key){
+            return machines[key];
+        });
+    //    console.log(keys);
+    //    console.log(values);
+        // console.log(values);
+        chart.destroy();
+        // charts(keys, values);
+        
+
+
+        /*  
         // a dictionary of machine and it's total time; "machineTime"
         var machineTime = {};
         machines.forEach((key, i) => machineTime[key] = totalTimes[i]);
@@ -105,94 +111,98 @@ function query_date(from, to, db){
                 var endTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
 
                 console.log(startTime + " - " + endTime);
-
             }   
         }
-        
-        chart.destroy();
-        charts(machines, totalTimes);
+        */
+
     })
 
 }
 
+function convertRawTimes(machines) {
+    var hours = {}; 
+    for (var i=0; i<24; i++) {
+        hours[i] = 0;
+    }
+    // console.log(hours);
+    for (value in machines) {
+        result = machines[value];
+    //    console.log(result);
+        var diff = 0;
+        for (var i=0; i<result.length; i++) {
+            res = result[i].split(/#/);
+            start = res[0];
+            end = res[1];
+            diff = (end - start) + diff;
+
+            // Create a new JavaScript Date object based on the timestamp
+            // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+            var date = new Date(start*1000);
+            // Hours part from the timestamp
+            var startHour = date.getHours();
+
+            // Create a new JavaScript Date object based on the timestamp
+            // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+            var date = new Date(end*1000);
+            // Hours part from the timestamp
+            var endHour = date.getHours();
+        }
+        hours[startHour] = diff;
+        console.log(hours);
+        console.log(value);
+        machines[value] = hours;
+        console.log(machines[value]);
+        console.log(machines);
+    //    console.log(start, end, diff);
+    }
+    return(machines);
+}
 
 
-function charts(machines, totalTimes){
-    
+
+function charts(keys, values){    
     /* ChartJS
     * -------
     * Data and config for chartjs
     */
     'use strict';
-    var data = {
-        labels: machines,
-        datasets: [{
-        label: "# of seconds",
-        backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
-            'rgba(25, 99, 132, 0.2)',
-            'rgba(5, 162, 235, 0.2)',
-            'rgba(25, 206, 86, 0.2)',
-            'rgba(7, 192, 192, 0.2)',
-            'rgba(15, 102, 255, 0.2)',
-            'rgba(25, 159, 64, 0.2)',
-        ],
-        borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)',
-            'rgba(25, 99, 132, 1)',
-            'rgba(5, 162, 235, 1)',
-            'rgba(25, 206, 86, 1)',
-            'rgba(7, 192, 192, 1)',
-            'rgba(15, 102, 255, 1)',
-            'rgba(25, 159, 64, 1)',
-        ],
-        data: totalTimes,
-        borderWidth: 1,
-        fill: false
-        }],
-    };
 
-    var multiLineData = {
-        labels: machines,
-        datasets: [{
-            label: machines[0],
-            data: totalTimes,
-            borderColor: [
-            '#587ce4'
-            ],
-            borderWidth: 2,
-            fill: false
-        },
-        {
-            label: machines[1],
-            data: totalTimes,
-            borderColor: [
-            '#ede190'
-            ],
-            borderWidth: 2,
-            fill: false
-        },
-        {
-            label: machines[2],
-            data: totalTimes,
-            borderColor: [
-            '#f44252'
-            ],
-            borderWidth: 2,
-            fill: false
-        }
-        ]
-    };
+    var latP1 = {
+        label:keys[0], //Object.keys(machines[0]),
+        data: values[0],
+        borderColor: [
+        '#587ce4'
+        ],
+        borderWidth: 2,
+        fill: false
+    }
+
+    var latP2 = {
+        label: keys[1],
+        data: values[1],
+        borderColor: [
+        '#ede190'
+        ],
+        borderWidth: 2,
+        fill: false
+    }
+
+    var triceps = {
+        label: keys[2],
+        data: values[2],
+        borderColor: [
+        '#f44252'
+        ],
+        borderWidth: 2,
+        fill: false
+    }
+
+    var data = {
+        labels: ['00:00','01:00', '02:00', '03:00', '04:00', '05:00', 
+        '06:00','07:00','08:00','09:00','10:00','11:00',
+        '12:00','13:00','14:00','15:00','16:00','17:00','18:00','18:00', '20:00', '21:00', '22:00', '23:00', '24:00'],
+        datasets: [latP1, latP2, triceps]
+    }
 
     var options = {
         responsive: true,
@@ -214,7 +224,7 @@ function charts(machines, totalTimes){
         xAxes: [{
             scaleLabel: {
                 display: true,
-                labelString: "Machines"
+                labelString: "Time"
             }
         }], 
         },
@@ -235,10 +245,9 @@ function charts(machines, totalTimes){
         // This will get the first returned node in the jQuery collection.
         chart = new Chart(chartCanvas, {
         type: 'line',
-        data: multiLineData,
+        data: data,
         options: options
         });
-
     }
 
     drawChart();
